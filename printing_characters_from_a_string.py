d268dddff3f35e28e91d61ccb354f1c24d3d1945
veronbungag <rveron.bungag@gmail.com>
2024-01-23 22:19:11 +0800

    pseudocode

---
+++ printing_characters_from_a_string.py
@@ -0,0 +1,6 @@
+#define a function to get characters from even index
+#for loop
+#accepting user input for a string
+#getting the characters from even index
+#printing the chracters from even index
+#for adding newline character after each character
