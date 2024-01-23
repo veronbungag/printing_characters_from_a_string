45185a8b773b9fe26bfa4bde0afc907dca0176ce
veronbungag <rveron.bungag@gmail.com>
2024-01-23 22:33:44 +0800

    done getting characters

--- printing_characters_from_a_string.py
+++ printing_characters_from_a_string.py
@@ -10,5 +10,6 @@ def even_index_characters(input_string):
 #accepting user input for a string
 input_string = input("Please enter a string: ")
 #getting the characters from even index
+even_index_characters = even_index_characters(input_string)
 #printing the chracters from even index
 #for adding newline character after each character
